b52f23bb9ed2a76e3c41e5b7047889a438532dcd
Jon Riquer <jonriquer@gmail.com>
2019-03-27 11:33:30 -0400

    began adding matrix for obstacles and rover 2

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,3 @@
-// Rover Object Goes Here
 var rover = {
   direction: "N", 
   x: 0,
@@ -37,19 +36,41 @@ function turnRight(rover){
 }
 
 function moveForward(rover) {
-  if (rover.x < 10 && rover.y < 10) { //10x10 Boundraries
-  switch (rover.direction) {
-    case "N": rover.y -= 1;
-    break;
-    case "E": rover.x += 1;
-    break;
-    case "S": rover.y += 1;
-    break;
-    case "W": rover.x -= 1;
-    break;
+  function fieldCheck () {
+    for (r=0; r < field.length; r++) {
+      var row = field[r];
+      for (c=0; c < row.length; c++){
+        var column = row[c];
+        if (column === "O" || rover2) {
+          console.log("Obstacle ahead @" + field[r][c] + ", Turn!");
+          var clear = false;
+        }
+      }
+    }
+  }
+  fieldCheck()
+  if (clear === false) {
+  
+  }
+  //10x10 Boundraries
+  if (rover.x < 9 && rover.y < 9) { 
+    switch (rover.direction) {
+      case "N": rover.y -= 1;
+      break;
+      case "E": rover.x += 1;
+      break;
+      case "S": rover.y += 1;
+      break;
+      case "W": rover.x -= 1;
+      break;
     }
     rover.travelLog.push(rover.x);
     rover.travelLog.push(rover.y);
+
+    var currentRow = rover.y;
+    var currentCol = rover.x;
+
+    field[currentRow][currentCol].push(rover.x, rover.y);
     console.log(rover);
   }
   else console.log("Cant go Offgrid! Please BACKUP!");
@@ -65,17 +86,16 @@ function moveBackward(rover) {
     break;
     case "W": rover.x += 1;
     break;
-    }
-    rover.travelLog.push(rover.x);
-    rover.travelLog.push(rover.y);
-    console.log(rover);
+  }
+  rover.travelLog.push(rover.x);
+  rover.travelLog.push(rover.y);
+  console.log(rover);
 }
 
 function commands(commandList) {
   // Input Validation 
   for (i=0; i < commandList.length; i++) {
-    if (commandList.charAt(i) != "f" && "r" && "l") 
-      var validInputs = false; 
+    if (commandList.charAt(i) != "f" && "r" && "l" && "b") validInputs = false;
     else validInputs = true; 
   }
   // Main Function Execution
@@ -88,7 +108,31 @@ function commands(commandList) {
         break;
         case "l": turnLeft(rover);
         break;
+        case "b": moveBackward(rover);
       }
     }
-  else console.log("INVALID!")
+  else console.log("INVALID Input!")
+}
+
+var field = [
+    //0   //1   //2   //3   //4   //5   //6   //7   //8   //9
+  [rover, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, "O", null, null],
+  [null, null, "O", null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, "O", null, null, null, null, null],
+  [null, null, null, null, null, null, rover2, null, null, null],
+  [null, "O", null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, "O", null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null]
+]
+
+
+
+var rover2 = {
+  direction: "W", 
+  x: 6,
+  y: 6,
+  travelLog: [],
 }
